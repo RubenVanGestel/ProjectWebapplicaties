846af76df0e6e92f27badf19721ee50764be12be
RubenVanGestel <r0900368@student.thomasmore.be>
2022-12-07 20:17:48 +0100

    Database Aangemaakt

---
+++ ProjectWebapplicaties/Data/ProjectWebapplicatiesContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjectWebapplicaties.Models;
+
+namespace ProjectWebapplicaties.Data
+{
+    public class ProjectWebapplicatiesContext : DbContext
+    {
+        public ProjectWebapplicatiesContext(DbContextOptions<ProjectWebapplicatiesContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Klant> Klanten { get; set; }
+        public DbSet<Medewerker> Medewerkers { get; set; }
+        public DbSet<Bestelling> Bestellingen { get; set; }
+        public DbSet<Status> Statussen { get; set; }
+        public DbSet<PizzaGrootte> PizzaGroottes { get; set; }
+        public DbSet<BestellingPizza> BestellingPizzas { get; set; }
+        public DbSet<Pizza> Pizzas { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Klant>().ToTable("Klanten");
+            modelBuilder.Entity<Medewerker>().ToTable("Medewerkers");
+            modelBuilder.Entity<Bestelling>().ToTable("Bestellingen");
+            modelBuilder.Entity<Status>().ToTable("Statussen");
+            modelBuilder.Entity<PizzaGrootte>().ToTable("PizzaGroottes");
+            modelBuilder.Entity<BestellingPizza>().ToTable("BestellingPizzas");
+            modelBuilder.Entity<Pizza>().ToTable("Pizzas");
+        }
+
+
+    }
+}
